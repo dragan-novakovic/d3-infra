486803cb3ae8ed42c37c495740531fbe90b00256
Dragan <Dragan>
2024-11-17 22:58:25 +0100

    qq

--- 3dmarketplace/Program.cs
+++ 3dmarketplace/Program.cs
@@ -1,24 +1,39 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-;
-
-var builder = WebApplication.CreateBuilder(args);
+using Npgsql;
 
 
-builder.Services.AddAuthentication();
-builder.Services.AddControllers();
-builder.Services.AddDbContext<UserDataContext>(options =>
+var connString = "Host=192.168.1.11;Username=casaos;Password=casaos;Database=casaos";
+
+await using var conn = new NpgsqlConnection(connString);
+await conn.OpenAsync();
+
+var tables = new List<string>();
+string query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'";
+using (var command = new NpgsqlCommand(query, conn))
 {
-    options.UseSqlServer("Data Source=.;Initial Catalog=Identity;Integrated Security=True");
-});
-builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<UserDataContext>();
+    using (var reader = command.ExecuteReader()) { while (reader.Read()) { tables.Add(reader.GetString(0)); } }
+}
+Console.WriteLine("Tables: " + string.Join(", ", tables));
 
 
-var app = builder.Build();
+// var builder = WebApplication.CreateBuilder(args);
 
 
-app.MapIdentityApi<IdentityUser>();
-app.UseAuthorization();
-app.MapControllers();
+// builder.Services.AddAuthentication();
+// builder.Services.AddControllers();
+// builder.Services.AddDbContext<UserDataContext>(options =>
+// {
+//     options.Usen("Data Source=.;Initial Catalog=Identity;Integrated Security=True");
+// });
+// builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<UserDataContext>();
 
-app.Run();
+
+// var app = builder.Build();
+
+
+// app.MapIdentityApi<IdentityUser>();
+// app.UseAuthorization();
+// app.MapControllers();
+
+// app.Run();
