5d66ca2070dd74d5582ea6f5ce1f9ff4c73fe468
Dragan <Dragan>
2025-02-09 11:20:51 +0100

    qq

--- 3dmarketplace/obj/Debug/net9.0/3dmarketplace.AssemblyInfo.cs
+++ 3dmarketplace/obj/Debug/net9.0/3dmarketplace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3dmarketplace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24e26d20a047921823513e0fd40d1eb887267010")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a4aa5ac323d92353101c9f1a5dfb54bcffb3c8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("3dmarketplace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3dmarketplace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
